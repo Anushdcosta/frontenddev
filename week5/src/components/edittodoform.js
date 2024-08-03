374b64f318a1c9edb1020dece33a040e74a129fc
Anushdcosta <Anushviston@gmail.com>
2024-08-03 12:18:55 +0400

    week5

--- week5/src/components/edittodoform.js
+++ week5/src/components/edittodoform.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const edittodoform = () => {
+export const EditTodoform = ({editTodo, task}) => {
+    const [value, setValue] = useState(task.task);
+
+    const handleSubmit = (e) => {
+      // prevent default action
+        e.preventDefault();   
+        // edit todo
+        editTodo(value, task.id);
+
+        setValue("")
+      };
   return (
-    <div>edittodoform</div>
+    <form onSubmit={handleSubmit} className="TodoForm">
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
+      <button type="submit" className='todo-btn'>Update Task</button>
+    </form>
   )
 }
--- week5/src/components/todoform.js
+++ week5/src/components/todoform.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const todoform = () => {
+export const Todoform = ({addTodo}) => {
+  const [value, setValue] = useState("")
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    addTodo(value)
+
+    setValue("")
+  }
   return (
-    <div>todoform</div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type="text" className='todo-input' value={value} placeholder='what is the task today' onChange={
+        (e) => setValue(e.target.value)
+      }/>
+      <button type='submit' className='todo-btn'>Add Task</button>
+    </form>
   )
 }
